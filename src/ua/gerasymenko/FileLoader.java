3e667db8f8547c6e2c787a63f75318294433daa9
Adam03179 <gerasymenko1983@gmail.com>
2018-01-10 00:30:10 +0100

    final version

--- src/ua/gerasymenko/FileLoader.java
+++ src/ua/gerasymenko/FileLoader.java
@@ -3,45 +3,33 @@ package ua.gerasymenko;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class FileLoader {
 
     private  String filename;
     private  Map<Character, Set<String>> words = new HashMap<>();
 
-
     public FileLoader(String filename) {
         this.filename = filename;
     }
 
     public Map<Character, Set<String>> loadFileToMap() {
-
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-
             String sCurrentLine;
-
             while ((sCurrentLine = br.readLine()) != null) {
-
                 if (words.containsKey(sCurrentLine.charAt(0))) {
                     words.get(sCurrentLine.charAt(0)).add(sCurrentLine);
                 } else {
                     words.put(sCurrentLine.charAt(0), new HashSet<>());
                     words.get(sCurrentLine.charAt(0)).add(sCurrentLine);
                 }
-
             }
-
         } catch (IOException e) {
+            System.err.println(String.format("error reading from file %s", filename));
             e.printStackTrace();
         }
-
         return words;
-
-
     }
 
 
--- src/ua/gerasymenko/HandlerOfConcatenatedWord.java
+++ src/ua/gerasymenko/HandlerOfConcatenatedWord.java
@@ -1,41 +1,31 @@
 package ua.gerasymenko;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class HandlerOfConcatenatedWord {
 
-    private  String firstLongestConcatWord = "";
-    private  String secondLongestConcatWord = "";
-    private  Map<Character, Set<String>> words;
-    private  boolean isRecursiveFlag = false;
-    private  boolean isConcatenatedFlag = false;
+    private String firstLongestConcatWord = "";
+    private String secondLongestConcatWord = "";
+    private Map<Character, Set<String>> words;
+    private boolean isRecursive = false;
     private int numberOfConcatenatedWords = 0;
-
+    private List<String> cutWord = new LinkedList<>();
 
     public HandlerOfConcatenatedWord(Map<Character, Set<String>> words) {
-
         this.words = words;
     }
 
-
     public void handle() {
-
-
         for (Map.Entry<Character, Set<String>> entry : words.entrySet()) {
-
             for (String word : entry.getValue()) {
-                isRecursiveFlag = false;
-                isConcatenatedFlag = false;
+                isRecursive = false;
                 if (isConcatenated(word)) {
                     numberOfConcatenatedWords++;
                     checkForFirstPlaces(word);
                 }
             }
         }
-
-     }
+    }
 
     public String getFirstLongestConcatWord() {
         return firstLongestConcatWord;
@@ -50,38 +40,41 @@ public class HandlerOfConcatenatedWord {
     }
 
     private void checkForFirstPlaces(String word) {
-
         if (firstLongestConcatWord.length() < word.length()) {
             secondLongestConcatWord = firstLongestConcatWord;
             firstLongestConcatWord = word;
         } else if (secondLongestConcatWord.length() < word.length()) {
             secondLongestConcatWord = word;
         }
-
     }
 
     private boolean isConcatenated(String word) {
-
+        boolean result = false;
         if (words.get(word.charAt(0)) == null) {
             return false;
         }
         Set<String> tempSet = new HashSet<>(words.get(word.charAt(0)));
 
-        if(!isRecursiveFlag){
+        if (!isRecursive) {
             tempSet.remove(word);
         }
 
         for (String str : tempSet) {
             if (word.startsWith(str)) {
+                cutWord.add(word.substring(0, str.length()));
                 word = word.substring(str.length());
                 if (word.length() == 0) {
-                    isConcatenatedFlag = true;
-                }else {
-                    isRecursiveFlag = true;
-                    isConcatenated(word);
+                    return true;
+                } else {
+                    isRecursive = true;
+                    result = isConcatenated(word);
+                    if (!result && !cutWord.isEmpty()) {
+                        word = cutWord.get(cutWord.size() - 1) + word;
+                        cutWord.remove(cutWord.size() - 1);
+                    }
                 }
             }
         }
-        return isConcatenatedFlag;
+        return result;
     }
 }
